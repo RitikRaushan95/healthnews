10393b2fdc60d6c48be291c8990c0fd0d6f53195
Ritik <ritikraushan160@gmail.com>
2024-04-10 00:05:32 +0530

    commit

--- src/Main.js
+++ src/Main.js
@@ -8,9 +8,7 @@ const fetchNews=async ()=>{
     const news=await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=f9de620adab044ea852839ab79673f0b");
     const randomIndex = Math.floor(Math.random() *10);
     const newsData=news.data.articles[randomIndex];
-    console.log(typeof(newsData));
     newsSet(newsData);
-    console.log(newsData);
 }
 
 useEffect(() => {
