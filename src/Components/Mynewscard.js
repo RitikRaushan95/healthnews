09084f030a27aba4e6ea8a625919b56372c2c2d2
Ritik <ritikraushan160@gmail.com>
2024-04-16 14:57:13 +0530

    commit

--- src/Components/Mynewscard.js
+++ src/Components/Mynewscard.js
@@ -1,46 +1,16 @@
 import React from 'react'
 
-function Mynewscard({newsCollection,newsCollection1,newsCollection2,newsCollection3}) {
+function Mynewscard({newsCollection}) {
+  console.log(newsCollection.urlToImage)
   return (
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src={newsCollection.urlToImage} alt="API fetch error"/>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{newsCollection.title}</h2>
-          <p class="leading-relaxed text-base">{newsCollection.description}</p>
-          <a rel='noreferrer' className='text-blue-500 hover:text-blue-700 underline' href={newsCollection.url} target='_blank'>Know More</a>
-
-        </div>
-      </div>
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src={newsCollection1.urlToImage} alt="API fetch error"/>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{newsCollection1.title}</h2>
-          <p class="leading-relaxed text-base">{newsCollection1.description}</p>
-          <a rel='noreferrer' className='text-blue-500 hover:text-blue-700 underline' href={newsCollection1.url} target='_blank'>Know More</a>
-        </div>
-      </div>
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src={newsCollection2.urlToImage} alt="API fetch error"/>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{newsCollection2.title}</h2>
-          <p class="leading-relaxed text-base">{newsCollection2.description}</p>
-          <a rel='noreferrer' className='text-blue-500 hover:text-blue-700 underline' href={newsCollection2.url} target='_blank'>Know More</a>
-        </div>
-      </div>
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src={newsCollection3.urlToImage} alt="API fetch error"/>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{newsCollection3.title}</h2>
-          <p class="leading-relaxed text-base">{newsCollection3.description}</p>
-          <a rel='noreferrer' className='text-blue-500 hover:text-blue-700 underline' href={newsCollection3.url} target='_blank'>Know More</a>
-        </div>
-      </div>
+    <div className="xl:w-1/4 md:w-1/3 p-4">
+    <div className="bg-gray-100 p-6 rounded-lg">
+        <img className="h-40 rounded w-full object-cover object-center mb-6" src={newsCollection.urlToImage} alt="API fetch error" />
+        <h2 className="text-lg text-gray-900 font-medium title-font mb-4 overflow-hidden line-clamp-2" >{newsCollection.title}</h2>
+        <p className="leading-relaxed text-base overflow-hidden line-clamp-4">{newsCollection.description}</p>
+        <a rel='noreferrer' className='text-blue-500 hover:text-blue-700 underline' href={newsCollection.url} target='_blank'>Know More</a>
     </div>
-  </div>
-</section>
+</div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import Main from './Main';
 import './index.css'
+import Clone from './Clone';
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Main/>
+    <Clone/>
 )
