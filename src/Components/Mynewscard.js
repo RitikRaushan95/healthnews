13d8f48cf11b40837605800d40291639a5be4e92
Ritik <ritikraushan160@gmail.com>
2024-04-17 18:51:19 +0530

    commit

--- src/Components/Mynewscard.js
+++ src/Components/Mynewscard.js
@@ -3,10 +3,16 @@ import React from 'react'
 function Mynewscard({newsCollection}) {
   console.log(newsCollection.urlToImage)
   return (
-    <div className="xl:w-1/4 md:w-1/3 p-4">
+    <div className="xl:w-1/4 md:w-1/3 p-4 ">
     <div className="bg-gray-100 p-6 rounded-lg">
-        <img className="h-40 rounded w-full object-cover object-center mb-6" src={newsCollection.urlToImage} alt="API fetch error" />
-        <h2 className="text-lg text-gray-900 font-medium title-font mb-4 overflow-hidden line-clamp-2" >{newsCollection.title}</h2>
+    <img
+  className="h-40 rounded w-full object-cover object-center mb-6 transition duration-300 ease-in-out transform hover:opacity-80 hover:shadow-md"
+  src={newsCollection.urlToImage}
+  alt="API fetch error"
+/>
+
+
+
         <p className="leading-relaxed text-base overflow-hidden line-clamp-4">{newsCollection.description}</p>
         <a rel='noreferrer' className='text-blue-500 hover:text-blue-700 underline' href={newsCollection.url} target='_blank'>Know More</a>
     </div>
--- src/Main.js
+++ src/Main.js
@@ -4,25 +4,25 @@ import Mynewscard from './Components/Mynewscard';
 function Main() {
     const [news,newsSet]=useState([]);
 
-    let newsStore=news.map((ele,index)=>{
-          return <Mynewscard key={index} newsCollection={ele}/>
+    const newsStore=news.map((ele,index)=>{
+      return <Mynewscard key={index} newsCollection={ele}/>
     })
 
-    const fetchNews = async () => {
-          const news = await fetch("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=f9de620adab044ea852839ab79673f0b");
-          const json= await news.json();
-          const newsData = json.articles;
-          newsSet(newsData);
-      }
-    
+    const apiFunction= async()=>{
+            const url= await fetch("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=f9de620adab044ea852839ab79673f0b");
+            const jsonData= await url.json();
+            const finalData=jsonData.articles;
+            newsSet(finalData);
+    }
+
     useEffect(() => {
-        fetchNews();
+      apiFunction();
     }, []);
   
       return (
         <div className="container px-5 py-24 mx-auto flex flex-wrap justify-center">
-        {newsStore}
-    </div>
+         {newsStore}
+        </div>
       )
 }
 
